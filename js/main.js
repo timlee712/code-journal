4da6cc6006cf22207357255e5275a8e875453b9a
Timothy Lee <leetim1202@gmail.com>
2023-02-14 20:15:29 +0000

    Created a function named renderEntry that generates a DOM tree

--- js/main.js
+++ js/main.js
@@ -25,3 +25,34 @@ $form.addEventListener('submit', function (event) {
   $form.reset();
 
 });
+
+function renderEntry(entry) {
+  var listItem = document.createElement('li');
+  var entryDiv = document.createElement('div');
+  var entryImageDiv = document.createElement('div');
+  var entryImage = document.createElement('img');
+  var entryTextDiv = document.createElement('div');
+  var entryTitle = document.createElement('h3');
+  var entryNotes = document.createElement('p');
+
+  listItem.appendChild(entryDiv);
+  entryDiv.appendChild(entryImageDiv);
+  entryImageDiv.appendChild(entryImage);
+  entryDiv.appendChild(entryTextDiv);
+  entryTextDiv.appendChild(entryTitle);
+  entryTextDiv.appendChild(entryNotes);
+
+  listItem.className = 'entry row';
+  entryImageDiv.className = 'entry-image column-half';
+  entryTextDiv.className = 'entry-text column-half';
+  entryTitle.className = 'entry-title';
+  entryNotes.className = 'entry-notes';
+
+  entryImage.src = entry.photoURL;
+  entryImage.alt = 'entry image';
+  entryTitle.textContent = entry.title;
+  entryNotes.textContent = entry.notes;
+
+  return listItem;
+}
+renderEntry();
